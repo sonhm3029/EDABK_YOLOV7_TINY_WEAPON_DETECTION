53ac618dff3f083b44dbb0ca5eb2e85e9b1d84e1
sonhm2329 <hoangminhson29102001@gmail.com>
2022-11-06 18:46:12 +0700

    Ok

--- test.py
+++ test.py
@@ -1,78 +1,123 @@
-# Import packages
+# Import thư viện để nhận diện ảnh trong thực tế
+
+import os
+import sys
+import argparse
+import time
+from pathlib import Path
 import cv2
- 
-# Lists to store the bounding box coordinates
-top_left_corner=[]
-bottom_right_corner=[]
- 
-# function which will be called on mouse input
-def drawRectangle(action, x, y, flags, *userdata):
-  # Referencing global variables 
-  global top_left_corner, bottom_right_corner
-  # Mark the top left corner when left mouse button is pressed
-  if action == cv2.EVENT_LBUTTONDOWN:
-    top_left_corner = [(x,y)]
-    # When left mouse button is released, mark bottom right corner
-  elif action == cv2.EVENT_LBUTTONUP:
-    bottom_right_corner = [(x,y)]    
-    # Draw the rectangle
-    print(top_left_corner, bottom_right_corner)
-    cv2.rectangle(image, top_left_corner[0], bottom_right_corner[0], (0,255,0),2, 8)
-    cv2.imshow("Window",image)
- 
-# Read Images
-image = cv2.imread("background_1.jpg")
-print(image.shape)
-# Make a temporary image, will be useful to clear the drawing
-temp = image.copy()
-# Create a named window
-cv2.namedWindow("Window")
-# highgui function called when mouse events occur
-cv2.setMouseCallback("Window", drawRectangle)
- 
-k=0
-# Close the window when key q is pressed
-while k!=113:
-  # Display the image
-  cv2.imshow("Window", image)
-  k = cv2.waitKey(0)
-  # If c is pressed, clear the window, using the dummy image
-  if (k == 99):
-    image= temp.copy()
-    cv2.imshow("Window", image)
- 
-cv2.destroyAllWindows()
+import torch
+import numpy as np
+import torch.backends.cudnn as cudnn
+from numpy import random
 
-# # import PIL module
-# from PIL import Image
+from models.experimental import attempt_load
+from utils.datasets import LoadStreams, LoadImages
+from utils.general import check_img_size, check_requirements, check_imshow, non_max_suppression, apply_classifier, \
+    scale_coords, xyxy2xywh, strip_optimizer, set_logging, increment_path
+from utils.plots import plot_one_box
+from utils.torch_utils import select_device, load_classifier, time_synchronized, TracedModel
 
-# # Front Image
-# filename = 'assets/img/knife_1.png'
+def letterbox(img, new_shape=(640, 640), color=(114, 114, 114), auto=True, scaleFill=False, scaleup=True, stride=32):
+    # Resize and pad image while meeting stride-multiple constraints
+    shape = img.shape[:2]  # current shape [height, width]
+    if isinstance(new_shape, int):
+        new_shape = (new_shape, new_shape)
 
-# # Back Image
-# filename1 = 'background_1.jpg'
+    # Scale ratio (new / old)
+    r = min(new_shape[0] / shape[0], new_shape[1] / shape[1])
+    if not scaleup:  # only scale down, do not scale up (for better test mAP)
+        r = min(r, 1.0)
 
-# # Open Front Image
-# frontImage = Image.open(filename)
+    # Compute padding
+    ratio = r, r  # width, height ratios
+    new_unpad = int(round(shape[1] * r)), int(round(shape[0] * r))
+    dw, dh = new_shape[1] - new_unpad[0], new_shape[0] - new_unpad[1]  # wh padding
+    if auto:  # minimum rectangle
+        dw, dh = np.mod(dw, stride), np.mod(dh, stride)  # wh padding
+    elif scaleFill:  # stretch
+        dw, dh = 0.0, 0.0
+        new_unpad = (new_shape[1], new_shape[0])
+        ratio = new_shape[1] / shape[1], new_shape[0] / shape[0]  # width, height ratios
 
-# # Open Background Image
-# background = Image.open(filename1)
+    dw /= 2  # divide padding into 2 sides
+    dh /= 2
 
-# # Convert image to RGBA
-# background.show()
-# frontImage = frontImage.convert("RGBA")
+    if shape[::-1] != new_unpad:  # resize
+        img = cv2.resize(img, new_unpad, interpolation=cv2.INTER_LINEAR)
+    top, bottom = int(round(dh - 0.1)), int(round(dh + 0.1))
+    left, right = int(round(dw - 0.1)), int(round(dw + 0.1))
+    img = cv2.copyMakeBorder(img, top, bottom, left, right, cv2.BORDER_CONSTANT, value=color)  # add border
+    return img, ratio, (dw, dh)
+  
+classes_to_filter = None  #You can give list of classes to filter by name, Be happy you don't have to put class number. ['train','person' ]
+source_image_path = "./data/test/images/weapon_948.png"
 
-# # Convert image to RGBA
-# background = background.convert("RGBA")
+opt  = {
+    
+    "weights": "./yolov7/runs/train/exp5/weights/best.pt", # Path to weights file default weights are for nano model
+    "yaml"   : "./yolov7/data/dataset.yaml",
+    "img-size": 640, # default image size
+    "conf-thres": 0.1, # confidence threshold for inference.
+    "iou-thres" : 0.65, # NMS IoU threshold for inference.
+    "device" : '0',  # device to run our model i.e. 0 or 0,1,2,3 or cpu
+    "classes" : classes_to_filter  # list of classes to filter or None
 
-# # Calculate width to be at the center
-# width = (background.width - frontImage.width) // 2
+}
 
-# # Calculate height to be at the center
-# height = (background.height - frontImage.height) // 2
+with torch.no_grad():
+  weights, imgsz = opt['weights'], opt['img-size']
+  set_logging()
+  device = select_device(opt['device'])
+  half = device.type != 'cpu'
+  model = attempt_load(weights, map_location=device)  # load FP32 model
+  stride = int(model.stride.max())  # model stride
+  imgsz = check_img_size(imgsz, s=stride)  # check img_size
+  if half:
+    model.half()
 
-# # Paste the frontImage at (width, height)
-# background.paste(frontImage, (width, height), frontImage)
+  names = model.module.names if hasattr(model, 'module') else model.names
+  colors = [[random.randint(0, 255) for _ in range(3)] for _ in names]
+  if device.type != 'cpu':
+    model(torch.zeros(1, 3, imgsz, imgsz).to(device).type_as(next(model.parameters())))
 
-# # Save this image
-# background.save("new.png", format="png")
+  img0 = cv2.imread(source_image_path)
+  img = letterbox(img0, imgsz, stride=stride)[0]
+  img = img[:, :, ::-1].transpose(2, 0, 1)  # BGR to RGB, to 3x416x416
+  img = np.ascontiguousarray(img)
+  img = torch.from_numpy(img).to(device)
+  img = img.half() if half else img.float()  # uint8 to fp16/32
+  img /= 255.0  # 0 - 255 to 0.0 - 1.0
+  if img.ndimension() == 3:
+    img = img.unsqueeze(0)
+
+  # Inference
+  t1 = time_synchronized()
+  pred = model(img, augment= False)[0]
+
+  # Apply NMS
+  classes = None
+  if opt['classes']:
+    classes = []
+    for class_name in opt['classes']:
+
+      classes.append(opt['classes'].index(class_name))
+
+
+  pred = non_max_suppression(pred, opt['conf-thres'], opt['iou-thres'], classes= classes, agnostic= False)
+  t2 = time_synchronized()
+  for i, det in enumerate(pred):
+    s = ''
+    s += '%gx%g ' % img.shape[2:]  # print string
+    gn = torch.tensor(img0.shape)[[1, 0, 1, 0]]
+    if len(det):
+      det[:, :4] = scale_coords(img.shape[2:], det[:, :4], img0.shape).round()
+
+      for c in det[:, -1].unique():
+        n = (det[:, -1] == c).sum()  # detections per class
+        s += f"{n} {names[int(c)]}{'s' * (n > 1)}, "  # add to string
+    
+      for *xyxy, conf, cls in reversed(det):
+
+        label = f'{names[int(cls)]} {conf:.2f}'
+        plot_one_box(xyxy, img0, label=label, color=colors[int(cls)], line_thickness=3)
